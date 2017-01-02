8e1248a6c9b8e5219c99aea7368a9433dad91463
ssarcandy <ssarcandy@gmail.com>
2017-01-02 23:35:51 +0800

    init

---
+++ main.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import cv2
+import os
+import numpy as np
+
+FILE_ROOT = os.getcwd()
+
+
+def main():
+    radius = 8
+    offset = 0  # 0~63
+    original_img = cv2.imread('./lightfield-images/lfc-dragons.dgauss-1200.tiff')
+
+    size = 150, 150, 3
+    m = np.zeros(size, dtype=np.uint8)
+
+    while offset < 64:
+        for y in range(m.shape[0]):
+            for x in range(m.shape[1]):
+                offsetX = offset % radius
+                offsetY = offset / radius
+                m[y][x] = original_img[radius * y + offsetX][radius * x + offsetY]
+        cv2.imwrite(os.path.join('refocused', 'refocus-' + `offsetX` + '_' + `offsetY` + '.tiff'), m)
+        offset += 1
+
+
+if __name__ == '__main__':
+    main()
